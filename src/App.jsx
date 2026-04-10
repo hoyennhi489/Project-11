8bce14fab486168476abaef8725ffe9818f8b3a7
hoyennhi489 <hyn489@gmail.com>
2026-04-10 13:47:42 +0700

    Initial setup for debug & practice project

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [books, setBooks] = useState([]);
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const handleAddBook = () => {
+    if (title.trim()) {
+      setBooks([...books, { title, author }]);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Favorite Books List</h1>
+      <input
+        type="text"
+        placeholder="Book Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Author"
+        value={author}
+        onChange={(e) => setAuthor(e.target.value)}
+      />
+      <button onClick={handleAddBook}>Add Book</button>
+      <ul>
+        {books.map((book, index) => (
+          <li key={index}>{book.title} by {book.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
